809abbbcc45b77eff355ef6fb82a2d1bc1c85ed7
abenitoc <vermibeto@gmail.com>
2015-11-19 15:29:35 +0100

    upload

--- script-python.py
+++ script-python.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python
 
 from lxml import etree
+from subprocess import call
+import shutil
+import os
+import sys
+
+param1 = sys.argv[1]
+
+#parse $1 python
+if param1 == 'create':
+	print 'n'	
+elif param1 == 'start': 
+	xml_path = "plantilla-vm-p3.xml"
+	xml_struct = etree.parse(xml_path)
+	print etree.tostring(xml_struct, pretty_print=True)
+elif param1 == 'stop':
+	print 'stop'
+elif param1 == 'destroy':
+	print 'l'
+else:
+	print 'Parametros no reconocidos' 
