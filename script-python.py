6583fa9ec5fb5f4e2f45ad9cecb1f7bbfa4ba699
abenitoc <vermibeto@gmail.com>
2015-11-19 16:02:51 +0100

    Added stuf

--- script-python.py
+++ script-python.py
@@ -5,12 +5,35 @@ from subprocess import call
 import shutil
 import os
 import sys
+import time
+
+############### AUXILIAR FUNCTIONS #############
+
+def relocateXML(name):
+	if not os.path.exists('machines'):
+		os.makedirs('machines')
+
+	shutil.copy2('/mnt/vnx/repo/plantilla-vm-p3.xml','machines/'+ name + '.xml')
+
+############### AUXILIAR FUNCTIONS #############
+
+################ MAIN FUNCTIONS ##############
+
+def create():
+	relocateXML('nueva')
+
+
+################ MAIN FUNCTIONS ##############
+
+################ PROGRAM DEFINITION ################
+start_time = time.time()
 
 param1 = sys.argv[1]
 
 #parse $1 python
 if param1 == 'create':
-	print 'n'	
+	create()
+	print 'Machines succesfully created'	
 elif param1 == 'start': 
 	xml_path = "plantilla-vm-p3.xml"
 	xml_struct = etree.parse(xml_path)
@@ -21,3 +44,6 @@ elif param1 == 'destroy':
 	print 'l'
 else:
 	print 'Parametros no reconocidos' 
+
+print("Finished in %s seconds" % (time.time() - start_time))
+################ PROGRAM DEFINITION ################
