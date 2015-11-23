d39ca0338c7573a9cc57578d09826afb30667212
abenitoc <vermibeto@gmail.com>
2015-11-23 15:41:33 +0100

    added python script

--- script-python.py
+++ script-python.py
@@ -9,19 +9,41 @@ import time
 
 ############### AUXILIAR FUNCTIONS #############
 
+def modifyXML(xml_path, vm_path, name, interface):	
+	xml_struct = etree.parse(path)
+	
+	root_node = xml_struct.getroot()
+	
+	machinename = root_node.find('name')
+	machinename.text = name
+	
+	disk_sourcefile = root.find('devices').find('disk').find('source')
+	disk_sourcefile.text = vm_path 
+	bridge_sourcefile =  root.find('devices').find('interface').find('source')	
+	bridge_source.text = interface
+
+	print etree.tostring(xml_struct, pretty_print=True)
+	
+
 def relocateXML(name):
+	#Base Cases
 	if not os.path.exists('machines'):
 		os.makedirs('machines')
-
+	if not os.file.exists('machines/cdps-practica.qcow2')	
+		shutil.call("bunzip /mnt/vnx/repo/cdps-vm-base3.bcow2.tar.gz .")
+	
+	
 	shutil.copy2('/mnt/vnx/repo/plantilla-vm-p3.xml','machines/'+ name + '.xml')
+
+def openVMs():
+	
 
 ############### AUXILIAR FUNCTIONS #############
 
 ################ MAIN FUNCTIONS ##############
 
 def create():
-	relocateXML('nueva')
-
+	relocateXML('1')	
 
 ################ MAIN FUNCTIONS ##############
 
@@ -32,12 +54,10 @@ param1 = sys.argv[1]
 
 #parse $1 python
 if param1 == 'create':
+
 	create()
 	print 'Machines succesfully created'	
 elif param1 == 'start': 
-	xml_path = "plantilla-vm-p3.xml"
-	xml_struct = etree.parse(xml_path)
-	print etree.tostring(xml_struct, pretty_print=True)
 elif param1 == 'stop':
 	print 'stop'
 elif param1 == 'destroy':
